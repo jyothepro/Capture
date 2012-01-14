5d91f4a9a420ef3d6f0bf73468d7e7becd640348
Jyothidhar Pulakunta <cooljyo@gmail.com>
2012-01-14 11:30:53 -0800

    Initial Commit

---
+++ Capture/CaptureAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CaptureAppDelegate.h
+//  Capture
+//
+//  Created by Jyothidhar Pulakunta on 1/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaptureAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Capture/CaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaptureViewController.h
+//  Capture
+//
+//  Created by Jyothidhar Pulakunta on 1/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaptureViewController : UIViewController
+
+@end
