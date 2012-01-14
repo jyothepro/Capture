f8860323d3710efc2db6ae939d628c75e23e2af8
Jyothidhar Pulakunta <cooljyo@gmail.com>
2012-01-14 14:03:39 -0800

    adding capture function

--- Capture/CaptureViewController.m
+++ Capture/CaptureViewController.m
@@ -61,5 +61,13 @@
 }
 
 - (IBAction)captureScreen:(id)sender {
+    NSLog(@"capture");
+    /*
+    UIGraphicsBeginImageContext(webView.frame.size);
+	[self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+	UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	UIImageWriteToSavedPhotosAlbum(viewImage, nil, nil, nil);
+     */
 }
 @end
